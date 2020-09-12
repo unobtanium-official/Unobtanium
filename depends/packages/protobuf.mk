98aad2c1325d1d71fadf98743dcca9a3999b79b5
Shinoa-Fores <37270226+Shinoa-Fores@users.noreply.github.com>
2020-09-12 12:13:59 -0400

    Update protobuf.mk
    Makefile fix for aarch64

--- depends/packages/protobuf.mk
+++ depends/packages/protobuf.mk
@@ -5,6 +5,11 @@ $(package)_file_name=$(native_$(package)_file_name)
 $(package)_sha256_hash=$(native_$(package)_sha256_hash)
 $(package)_dependencies=native_$(package)
 $(package)_cxxflags=-std=c++11
+$(package)_patches=protobuf_aarch64.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/protobuf_aarch64.patch
+endef
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --with-protoc=$(build_prefix)/bin/protoc
