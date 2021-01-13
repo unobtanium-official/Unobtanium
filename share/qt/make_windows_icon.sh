672661c826b87b65d3151f4d59e0355c6f64629e
Shinoa-Fores <btcinfo@sdf.org>
2021-01-13 16:06:59 -0500

    Final branding fixes to share/

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/unobtanium.png
+ICON_DST=../../src/qt/res/icons/unobtanium.ico
+convert ${ICON_SRC} -resize 16x16 unobtanium-16.png
+convert ${ICON_SRC} -resize 32x32 unobtanium-32.png
+convert ${ICON_SRC} -resize 48x48 unobtanium-48.png
+convert unobtanium-16.png unobtanium-32.png unobtanium-48.png ${ICON_DST}
 
