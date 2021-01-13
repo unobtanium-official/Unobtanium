75d50714a00140dacbe25a5f0c81db166565e724
Shinoa-Fores <btcinfo@sdf.org>
2021-01-13 15:43:48 -0500

    Trivial fixes to qa tools

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -2,11 +2,11 @@
 set -e
 
 CURDIR=$(cd $(dirname "$0"); pwd)
-# Get BUILDDIR and REAL_BITCOIND
+# Get BUILDDIR and REAL_UNOBTANIUMD
 . "${CURDIR}/tests-config.sh"
 
-export BITCOINCLI=${BUILDDIR}/qa/pull-tester/run-bitcoin-cli
-export BITCOIND=${REAL_BITCOIND}
+export UNOBTANIUMCLI=${BUILDDIR}/qa/pull-tester/run-unobtanium-cli
+export UNOBTANIUMD=${REAL_UNOBTANIUMD}
 
 if [ "x${EXEEXT}" = "x.exe" ]; then
   echo "Win tests currently disabled"
@@ -29,7 +29,7 @@ testScripts=(
     'zapwallettxes.py'
 #    'forknotify.py'
 );
-if [ "x${ENABLE_BITCOIND}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
+if [ "x${ENABLE_UNOBTANIUMD}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
     for (( i = 0; i < ${#testScripts[@]}; i++ ))
     do
         if [ -z "$1" ] || [ "$1" == "${testScripts[$i]}" ] || [ "$1.py" == "${testScripts[$i]}" ]
@@ -39,5 +39,5 @@ if [ "x${ENABLE_BITCOIND}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
         fi
     done
 else
-  echo "No rpc tests to run. Wallet, utils, and bitcoind must all be enabled"
+  echo "No rpc tests to run. Wallet, utils, and unobtaniumd must all be enabled"
 fi
