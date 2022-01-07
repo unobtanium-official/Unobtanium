25ae3abf6c9fac9b8b91a857bdbdf205da8e1f67
Shinoa-Fores <btcinfo@sdf.org>
2022-01-07 13:42:24 -0500

    native_b2 fix for boost update

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -19,6 +19,8 @@ upnp_packages=miniupnpc
 
 zmq_packages=zeromq
 
+$(host_arch)_$(host_os)_native_packages += native_b2
+
 ifneq ($(build_os),darwin)
 darwin_native_packages=native_cctools native_cdrkit native_libdmg-hfsplus
 endif
